5410ddeb1fd4bba5392ede33f9c7a8bb062b707b
Pepe Doval <pepellou@gmail.com>
2012-12-12 19:11:53 +0100

    N=2 - green

--- src/Wirth.php
+++ src/Wirth.php
@@ -5,7 +5,11 @@ class Wirth {
 	public static function getStrings(
 		$n
 	) {
-		return array( "A", "B", "C" );
+		if ($n == 1) {
+			return array( "A", "B", "C" );
+		} else {
+			return array( "AB", "AC", "BA", "BC", "CA", "CB" );
+		}
 	}
 
 }
