8d55daac6cf99f13083fb7004b3a4663704fc61b
Pepe Doval <pepellou@gmail.com>
2012-12-12 19:06:06 +0100

    Solves N=1 - red

---
+++ test/WirthTest.php
@@ -0,0 +1,13 @@
+<?php
+
+class WirthTest extends PHPUnit_Framework_TestCase {
+
+	public function test_it_solves_n_eql_1(
+	) {
+		$this->assertEquals(
+			array( "A", "B", "C" ),
+			Wirth::getStrings(1)
+		);
+	}
+
+}
