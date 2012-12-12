b89952655c00ef0b857bfb281c025fc2fa5525fc
Pepe Doval <pepellou@gmail.com>
2012-12-12 19:12:43 +0100

    N=2 - refactor

--- src/Wirth.php
+++ src/Wirth.php
@@ -2,11 +2,13 @@
 
 class Wirth {
 
+	private static $alphabet = array("A", "B", "C");
+
 	public static function getStrings(
 		$n
 	) {
 		if ($n == 1) {
-			return array( "A", "B", "C" );
+			return self::$alphabet;
 		} else {
 			return array( "AB", "AC", "BA", "BC", "CA", "CB" );
 		}
