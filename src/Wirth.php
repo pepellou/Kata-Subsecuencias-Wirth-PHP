f8aa4cece5ff2ca76b99bf9998d5fec17edeb422
Pepe Doval <pepellou@gmail.com>
2012-12-12 19:07:10 +0100

    Solves N=1 - green

---
+++ src/Wirth.php
@@ -0,0 +1,11 @@
+<?php
+
+class Wirth {
+
+	public static function getStrings(
+		$n
+	) {
+		return array( "A", "B", "C" );
+	}
+
+}
--- test/WirthTest.php
+++ test/WirthTest.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once (dirname(__FILE__)."/../src/Wirth.php");
+
 class WirthTest extends PHPUnit_Framework_TestCase {
 
 	public function test_it_solves_n_eql_1(
